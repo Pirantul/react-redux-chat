c84c3429dac609b9c97dd7afe1349f1c2e05b9ab
user <user@iMac-user.local>
2020-12-02 21:49:47 +0300

    change style

--- src/components/chat/Chat-item.js
+++ src/components/chat/Chat-item.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import 'font-awesome/css/font-awesome.min.css';
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 1,
-  },
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 1,
+    },
 });
 
 export default function OutlinedCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
 
-  return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        
-      </CardContent>
-     
-    </Card>
-  );
+    return (
+        <li className="clearfix">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_04.jpg" alt="avatar" />
+        <div className="about">
+          <div className="name">Erica Hughes</div>
+          <div className="status">
+            <i className="fa fa-circle online"></i> online
+          </div>
+        </div>
+      </li>
+
+
+    );
 }
